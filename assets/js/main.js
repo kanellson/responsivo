7cbc13d062174fe602f963f6b1e0063f3f0c118f
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-20 17:47:20 -0300

    refactor: remove estilos css que ocultavam o submenu full e adiciona evento javascript no span que ao clicar chama função para abrir/ocultar o submenu full, adicionando/removendo uma classe css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,26 @@
 
 const btnMenuMobile = document.querySelector('.btn-menu-mobile');
 const navigationNav = document.querySelector('.navigation-nav');
+const submenuFull = document.querySelector('.show-menu');
+const closeFullMenu = document.querySelector('.close-full-menu');
+const fullMenuSecundary = document.querySelector('.full-menu-secundary');
 
 btnMenuMobile.addEventListener('click', () => {
    navigationNav.classList.toggle('toggle-menu-mobile');
 });
 
-const closeFullMenu = document.querySelector('.close-full-menu');
-const fullMenuSecundary = document.querySelector('.full-menu-secundary');
+
+const openSubmenuFull = () => {
+  fullMenuSecundary.classList.toggle('hidden-menu');
+}
+
+submenuFull.addEventListener('click', openSubmenuFull)
+
 
 
 closeFullMenu.addEventListener('click', () => {
-   
-   fullMenuSecundary.classList.add('sub-full-menu');
+
+  fullMenuSecundary.classList.add('hidden-menu');
    // window.location.href = window.location.href
 
 });
